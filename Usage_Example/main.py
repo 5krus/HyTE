abc767480bbcc8325aa2c71a4cc98808a15374e4
Eryk Krusinski <ek620@cam.ac.uk>
2025-02-14 00:51:37 +0100

    Fixed Pylint

--- Usage_Example/main.py
+++ Usage_Example/main.py
@@ -5,9 +5,9 @@ This script shows how to use HyTE with a practical example.
 """
 
 # Prepare imports.
-from hyte import Iterator            # LLM-based Hypothesize-Test-Evaluate iterator.
 import tools                         # Functions, and their descriptions, for LLMs to use.
 import controls as ctl               # Prompts, tools, keys, and other consumables used by iterator.
+from hyte import Iterator            # LLM-based Hypothesize-Test-Evaluate iterator.
 
 # Run the iterator.
 [summary, full_log] = Iterator(ctl.OPTIONS).run(ctl.SYSTEM_PROMPTS,  # pylint: disable=not-callable.
